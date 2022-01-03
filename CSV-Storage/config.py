b2565cb036d4de894f3d3f6f0b16680f5de54d65
Aj Ishac <96703509+Aj-Ishac@users.noreply.github.com>
2022-01-03 05:57:10 -0600

    Add files via upload

---
+++ CSV-Storage/config.py
@@ -0,0 +1,19 @@
+#address = "https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308teagle-12gd/p/N82E16814932439"
+#address2 = "https://www.amazon.com/Amazon-Basics-microSDXC-Memory-Adapter/dp/B08TJTB8XS?ref_=ast_sto_dp&th=1&psc=1"
+
+
+
+header_values = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding':	'gzip, deflate',
+    'Accept-Language':	'en-US,en;q=0.9,hi;q=0.8',
+    'Connection':	'keep-alive',
+    # 'Host':	'www.amazon.com',
+    'Referer':	'https://www.google.com/',
+    'Upgrade-Insecure-Requests':	'1',
+    'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
+
+csv_folder = (r"E:\Personal Projects\scraper2\CSV storage")
+USER_NAME = "pricelookup.tool@gmail.com"
+USER_PASS = "pricelookup.tool1994"
