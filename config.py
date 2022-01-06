c5b963a7452029114e5b4ebfd2ebb67d80e8712f
Aj Ishac <96703509+Aj-Ishac@users.noreply.github.com>
2022-01-06 01:20:07 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 #address = "https://www.newegg.com/gigabyte-geforce-rtx-3080-ti-gv-n308teagle-12gd/p/N82E16814932439"
 #address2 = "https://www.amazon.com/Amazon-Basics-microSDXC-Memory-Adapter/dp/B08TJTB8XS?ref_=ast_sto_dp&th=1&psc=1"
 
-
-
 header_values = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'Accept-Encoding':	'gzip, deflate',
@@ -14,6 +12,8 @@ header_values = {
     'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
     }
 
-csv_folder = (r"E:\Personal Projects\scraper2\CSV storage")
+csv_folder = (r"E:\Personal Projects\scrappy\CSV storage")
+
+#gmail account needs to be created and the bottom two strings need to be filled with correct credentials
 USER_NAME = ""
 USER_PASS = ""
